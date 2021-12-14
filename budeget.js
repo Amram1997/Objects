11b99dfdcbd7fb7effd846c6d7be78d314cbf1e2
Amram1997 <amramananyan9@gmail.com>
2021-12-15 00:28:21 +0400

    feat add budget and resolved

---
+++ budeget.js
@@ -0,0 +1,11 @@
+function fiftyThirtyTwenty(number) {
+    return {
+        "Needs":(number * 50)/100,
+        "Wants":(number * 30)/100,
+        "Savings":(number * 20)/100
+
+    }
+}
+
+
+console.log(fiftyThirtyTwenty(50000));
